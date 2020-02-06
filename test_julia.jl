f63086cb508ffc392d1b1d2341830e2444fc4e08
newstatement <yan.b.bastos@gmail.com>
2020-02-06 08:02:31 -0300

    add exhaust heuristic

--- test_julia.jl
+++ test_julia.jl
@@ -7,3 +7,17 @@ function cputime(a,b,n)
 	t1 = now()
 	return [t1-t0, a]
 end
+
+function exhaust_heuristic_time(n,s)
+	t1 = now()
+	c = Array{Float64,1}(undef,factorial(big(n)))
+	for i = 1:factorial(big(n))
+		c[i] = 0.0
+		for j = 1:s
+			c[i] += rand()
+		end
+		c[i] /= s
+	end
+	t2 = now()
+	return [t2-t1,c]
+end
