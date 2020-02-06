7d5b2b0812b523bd1956c1f99456a2dc768195db
newstatement <yan.b.bastos@gmail.com>
2020-02-06 11:41:02 -0300

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,16 +1,16 @@
-import datetime
+import time
 import math
 from random import random
 
 def cputime(a,b,n):
-	t0 = datetime.datetime.now()
+	t0 = time.time_ns()
 	for i in range(n):
 		a /= b
-	t1 = datetime.datetime.now()
+	t1 = time.time_ns()
 	return [t1-t0, a]
 
 def exhaust_heuristic_time(n,s):
-	t0 = datetime.datetime.now()
+	t0 = time.time_ns()
 	p = math.factorial(n)
 	c = [0.0] * p
 	for i in range(p):
@@ -18,5 +18,15 @@ def exhaust_heuristic_time(n,s):
 		for j in range(s):
 			c[i] += random()
 		c[i] /= s
-	t1 = datetime.datetime.now()
+	t1 = time.time_ns()
 	return [t1-t0,c]
+
+def runbatch(n,p):
+	f = open("time_python.csv","w")
+	for i in range(n):
+		for j in range(p):
+			r = exhaust_heuristic_time(n,s)
+			f.write(r[0])
+			f.write(",")
+		f.write("\n")
+	f.close()
