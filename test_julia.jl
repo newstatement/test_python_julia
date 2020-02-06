7df91c54ac0321c1e3c446c612dc420f24da5e88
newstatement <yan.b.bastos@gmail.com>
2020-02-06 11:51:35 -0300

    Update test_julia.jl

--- test_julia.jl
+++ test_julia.jl
@@ -1,15 +1,15 @@
-using Dates
+
 function cputime(a,b,n)
-	t0 = now()
+	t0 = time_ns()
 	for i = 1:n
 		a /= b
 	end
-	t1 = now()
+	t1 = time_ns()
 	return [t1-t0, a]
 end
 
 function exhaust_heuristic_time(n,s)
-	t0 = now()
+	t0 = time_ns()
 	c = Array{Float64,1}(undef,factorial(big(n)))
 	for i = 1:factorial(big(n))
 		c[i] = 0.0
@@ -18,6 +18,19 @@ function exhaust_heuristic_time(n,s)
 		end
 		c[i] /= s
 	end
-	t1 = now()
+	t1 = time_ns()
 	return [t1-t0,c]
+end
+
+function runbatch(n,p)
+	f = open("time_julia.csv","w")
+	for i =1:n
+		for j = 1:p
+			r = exhaust_heuristic_time(n,s)
+			write(f,r[1])
+			write(f,",")
+		end
+		write(f,"\n")
+	end
+	close(f)
 end
