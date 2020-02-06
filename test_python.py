ed009d48be574b58b9b4dde903d2752d1457825c
newstatement <yan.b.bastos@gmail.com>
2020-02-06 08:16:16 -0300

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,7 +1,22 @@
 import datetime
+import math
+import random
+
 def cputime(a,b,n):
 	t0 = datetime.datetime.now()
 	for i in range(n):
 		a /= b
 	t1 = datetime.datetime.now()
 	return [t1-t0, a]
+
+def exhaust_heuristic_time(n,s):
+	t0 = datetime.datetime.now()
+	p = math.factorial(n)
+	c = range(p)
+	for i in range(p):
+		c[i] = 0.0
+		for j in range(s):
+			c[i] += uniform(0,1)
+		c[i] /= s
+	t1 = datetime.datetime.now()
+	return [t1-t0,c]
